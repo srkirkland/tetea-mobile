e83d861934962117d014b9a46ac8e4e96953f23d
Scott Kirkland <srkirkland@gmail.com>
2019-10-02 22:26:53 -0700

    some random links

---
+++ navigation/forms/FormFour.js
@@ -0,0 +1,44 @@
+const config = {
+  id: "form-four",
+  name: "Form Four Exams (CSEE)",
+  subjects: [
+    {
+      id: "civics",
+      name: "Civics",
+      docs: [
+        {
+          year: "2018",
+          name: "Questions",
+          uri:
+            "http://maktaba.tetea.org/past-papers/csee/civics/Civics%20-%20F4%20-%202018.pdf"
+        },
+        {
+          year: "2017",
+          name: "Questions",
+          uri:
+            "http://maktaba.tetea.org/past-papers/csee/civics/Civics%20-%20F4%20-%202017.pdf"
+        }
+      ]
+    },
+    {
+      id: "history",
+      name: "History",
+      docs: [
+        {
+          year: "2018",
+          name: "Questions",
+          uri:
+            "http://maktaba.tetea.org/past-papers/csee/history/History%20-%20F4%20-%202018.pdf"
+        },
+        {
+          year: "2017",
+          name: "Questions",
+          uri:
+            "http://maktaba.tetea.org/past-papers/csee/history/History%20-%20F4%20-%202017.pdf"
+        }
+      ]
+    }
+  ]
+};
+
+export default config;
