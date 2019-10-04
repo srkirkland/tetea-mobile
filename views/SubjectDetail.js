39cb91fa56af0b592fdefb7a33749e4df9026810
Scott Kirkland <srkirkland@gmail.com>
2019-10-04 16:06:39 -0700

    list of subject details

--- views/SubjectDetail.js
+++ views/SubjectDetail.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-import { Text } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+
+function Year(props) {
+    return (
+        <TouchableOpacity>
+            <Text>{props.year} - {props.name}</Text>
+        </TouchableOpacity>
+    );
+}
 
 export default function SubjectDetail(props) {
   const subject = props.navigation.state.params;
 
   console.log("subject detail", props);
-  return <Text>{subject.name}</Text>;
+  return (
+    <View>
+      <Text>{subject.name}</Text>
+      <FlatList
+        data={subject.docs}
+        renderItem={({ item }) => (
+          <Year {...item} onPress={() => {}} />
+        )}
+        keyExtractor={item => `${item.name}-${item.year}`}
+      />
+    </View>
+  );
 }
 
 SubjectDetail.navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.state.params.name,
-    };
+  return {
+    title: navigation.state.params.name
   };
+};
